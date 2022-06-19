dc7d1cbd55e83031b2055ac70f4277cbdf284644
ArtemHard <95387501+ArtemHard@users.noreply.github.com>
2022-06-19 22:56:07 +0300

    Modernization
    Create, Context, add useMemo for ContextValue.
    Devided the components (PhonesDetail) on 3 components PDcard && PDModal

--- app/src/components/Phones/PhonesDetail/PhonesDetail.jsx
+++ app/src/components/Phones/PhonesDetail/PhonesDetail.jsx
@@ -1,98 +1,40 @@
-import { useEffect, useState } from "react"
-import { useNavigate, useParams } from "react-router-dom"
-import Modal from "../../Modal/Modal"
-import Form from "../Form/Form"
+import { useContext } from "react"
+import { createContext } from "react"
+import { useMemo } from "react"
+import usePhonesDetail from "./hooks/usePhonesDetail"
+import usePhonesDetailModal from "./hooks/usePhonesDetailModal"
+import PhonesDetailCard from "./PhonesDetailCard/PhonesDetailCard"
+import PhonesDetailModal from "./PhonesDetailModal/PhonesDetailModal"
 
+
+const PhonesDetailContext = createContext()
 
 const PhonesDetail = () => {
-    const { phonesId } = useParams()
+   
 
-    const navigate = useNavigate({})
+  const {viewModal, openModal, closeModal} = usePhonesDetailModal()
 
-    const [viewModal, setViewModal] = useState(false)
+  const {phone, submitHandler} = usePhonesDetail(closeModal)
 
-    const [phone, setPhone] = useState({})
-
-    
-
-    useEffect(() => {
-        fetch(`http://localhost:3000/api/v1/phones/${phonesId}`) 
-        .then(response => response.json())
-        .then(dataFromServer => setPhone(dataFromServer))
-
-       
-    }, [phonesId]) 
-
-
-
-const openModal = () => {
-    setViewModal(true)
-}
-
-const closeModal = () => {
-    setViewModal(false)
-}
-
-const submitHandler = async (e) => {
-    e.preventDefault()
-
-    console.log()
-
-    const formData = Object.fromEntries(new FormData(e.target).entries())
-
-    const response = await fetch(`http://localhost:3000/api/v1/phones/${phone.id}`, {
-      method: 'PATCH',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(formData),
-    })
-
-    if (response.status === 200) {
-      const updatedPhoneFromServer = await response.json() // не приходит ответ
-      setPhone(updatedPhoneFromServer)
-      closeModal()
-      e.target.reset()
-    } else {
-      alert('Wrong data')
+  const sharedValues = useMemo(() => {
+    return {
+      viewModal, openModal, closeModal, phone, submitHandler,
     }
-  }
-
-const content = () => {
-    if (!phone.id) {
-        return <strong>Loading...</strong>
-    }
-    
+  }, [phone, viewModal])
+  
 
     return (
-        <>
-        <div className="card" style={{ width: '18rem' }}>
-          <img src={phone.pic} className="card-img-top" alt="..." />
-          <div className="card-body">
-            <h5 className="card-title">{phone.name}</h5>
-            <p className="card-text">{phone.phone}</p>
-            <button type="button" onClick={() => navigate(-1)} className="btn btn-primary mx-1">Go back</button>
-            <button type="button" onClick={openModal} className="btn btn-success mx-1">Edit</button>
-          </div>
+      <PhonesDetailContext.Provider value={sharedValues}>
+        <div className="d-flex justify-content-center">
+          <PhonesDetailCard />
+          <PhonesDetailModal />
         </div>
-        <Modal
-        state = {viewModal}
-        onClose = {closeModal}
-        >
-            <Form
-             onSubmit={submitHandler}   
-             {...phone}
-            />
-        </Modal>
-        </>
-    )
-}
-
-    return (
-        <div>
-            {content()}
-        </div>
+      </PhonesDetailContext.Provider>
+        
     )
 }
 
 export default PhonesDetail
+
+export const usePhonesDetailContext = () =>  useContext(PhonesDetailContext)
+
